8c2ddb0c47a7205d5331d50aa51c6ecdefbd6779
riansaunders <rian@riansaunders.com>
2022-07-16 09:18:05 -0400

    + Finish project setup + Add dist/ to .gitignore

--- dist/server/index.js
+++ dist/server/index.js
@@ -1,7 +1,22 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.nothing = void 0;
-function nothing() {
-    //
+require("dotenv/config");
+var express_1 = __importDefault(require("express"));
+var path_1 = __importDefault(require("path"));
+var app = (0, express_1.default)();
+var port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 8080;
+var isDev = process.env.NODE_ENV !== "production";
+if (isDev) {
+    app.use("/", express_1.default.static("../../dist/web"));
 }
-exports.nothing = nothing;
+else {
+    app.use("/", express_1.default.static("../web"));
+    console.log(path_1.default.join(__dirname, "web"));
+}
+app.listen(port, function () {
+    console.log("\u26A1\uFE0F[server]: Server is running at https://localhost:".concat(port));
+});
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,18 @@
 import "dotenv/config";
 import express, { Express, Request, Response } from "express";
+import { exec } from "child_process";
 
 const app: Express = express();
 const port = process.env.PORT ?? 8080;
+const isDev = process.env.NODE_ENV !== "production";
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+// Since the front-end is plain-jane js, we want to build the web
+// We will be using nodemon's reloading to achieve this.
+if (isDev) {
+  exec("npm run build:web");
+}
+
+app.use(express.static("dist/web"));
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);

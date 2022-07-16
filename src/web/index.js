c48e25c02d53905e18e3d77dae69259b8b2106d7
riansaunders <rian@riansaunders.com>
2022-07-16 13:42:21 -0400

    + Added upvotes and downvotes + Added a local userId - Removed upvotes from comments

--- src/web/index.js
+++ src/web/index.js
@@ -1,7 +1,15 @@
 /** @type {import("./scripts/jquery-3.6.0.min.js")} */
 import mustache from "./scripts/mustache.js";
 
-console.log(humanizeDuration);
+const upvoteButton = `<svg class="inline mr-1" width="11" height="10" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M0.431818 7L4.14205 0.818182L7.84943 7H0.431818Z" fill="#4B587C"/>
+</svg>
+<span>Upvote</span>`;
+
+const downvoteButton = ` <svg class="inline mr-1 rotate-180" width="11" height="10" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.431818 7L4.14205 0.818182L7.84943 7H0.431818Z" fill="#4B587C"/>
+</svg>
+<span>Downvote</span>`;
 
 const commentTemplate = `
   <div class="h-20 flex">
@@ -14,12 +22,20 @@ const commentTemplate = `
       <p class="text-sm">{{text}}</p>
       <div class="space-x-4 text-[#4B587C] text-sm font-semibold">
         <span>
-          <button>
 
-            <svg class="inline mr-1" width="11" height="10" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0.431818 7L4.14205 0.818182L7.84943 7H0.431818Z" fill="#4B587C"/>
-            </svg>
-            Upvote
+          <button data-commentid="{{id}}" data-upvoted="{{upvoted}}" class="vote-btn" onclick='toggleUpvote("{{id}}")'}> 
+
+          {{#upvoted?}}
+            ${upvoteButton}
+          {{/upvoted?}}
+
+          {{#upvoted}}
+            ${downvoteButton}
+          {{/upvoted}}
+
+            {{^upvoted}}
+              ${upvoteButton} 
+            {{/upvoted}} 
 
           </button>
         </span>
@@ -33,8 +49,76 @@ const commentTemplate = `
 `;
 
 $(function () {
+  let userId = localStorage.getItem("userId");
+  let upvotes = [];
+  if (!userId) {
+    console.log("...Getting new userId");
+    $.get("https://www.uuidtools.com/api/generate/v4", (data) => {
+      userId = data[0];
+      localStorage.setItem("userId", userId);
+      console.log(`Got it!`, userId);
+    });
+  }
+  console.log("UserId", userId);
+
   const commentContainer = $("#comments-container");
   const commentForm = $("#comment-form");
+  window.toggleUpvote = (commentId) => {
+    const btn = $(`[data-commentid="${commentId}"]`);
+    const upvoted = btn.attr("data-upvoted");
+
+    if (upvoted === "true") {
+      $.ajax({
+        url: "http://localhost:8080/downvote",
+        method: "POST",
+        dataType: "json",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: JSON.stringify({
+          commentId: commentId,
+          userId: userId,
+        }),
+
+        success: (data) => {
+          const upvoteIndx = upvotes.findIndex(
+            (u) => u.commentId === commentId && u.userId === userId
+          );
+          if (upvoteIndx !== -1) {
+            upvotes.splice(upvoteIndx, 1);
+          }
+          btn.attr("data-upvoted", false);
+
+          btn.html(upvoteButton);
+        },
+      });
+    } else {
+      $.post({
+        url: "http://localhost:8080/upvote",
+        method: "POST",
+        dataType: "json",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: JSON.stringify({
+          commentId: commentId,
+          userId: userId,
+        }),
+
+        success: (data) => {
+          upvotes.push(data.upvote);
+          btn.attr("data-upvoted", true);
+          btn.html($(downvoteButton));
+        },
+      });
+    }
+  };
+
+  $(".vote-btn").on("click", (e) => {
+    e.preventDefault();
+    const el = $(e.target);
+    alert("sSupupu");
+  });
 
   commentForm.on("submit", (e) => {
     e.preventDefault();
@@ -64,28 +148,51 @@ $(function () {
     });
   });
 
-  function createCommentElement({ image, name, text, upvotes, date }) {
-    const element = $(
-      mustache.render(commentTemplate, {
-        image: image,
-        name: name,
-        text: text,
-        upvotes: upvotes,
-        dateAdded: date,
-      })
-    );
+  function createCommentElement({
+    id,
+    image,
+    name,
+    text,
+    upvotes,
+    upvoted,
+    date,
+  }) {
+    try {
+      const element = $(
+        mustache.render(commentTemplate, {
+          image: image,
+          name: name,
+          text: text,
+          upvotes: upvotes,
+          dateAdded: date,
+          upvoted: upvoted,
+          id: id,
+        })
+      );
 
-    return element;
+      return element;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
 
   function addCommentFromWeb(comment) {
     const difference = Date.now() - new Date(comment.dateAdded).getTime();
 
+    console.log(upvotes, upvotes.findIndex((u) => u.userId === userId) !== -1);
     const element = createCommentElement({
+      id: comment.id,
       image: comment.author.avatarURL,
       name: comment.author.name,
       text: comment.text,
-      upvotes: comment.upvotes,
+      upvotes: upvotes.filter((u) => u.commentId === comment.id).length,
+      upvoted:
+        upvotes.findIndex(
+          (u) => u.userId === userId && u.commentId === comment.id
+        ) !== -1
+          ? true
+          : undefined,
       date:
         difference < 10000
           ? "moments"
@@ -99,9 +206,9 @@ $(function () {
   }
 
   function loadComments() {
-    $.getJSON("http://localhost:8080/comments")
+    $.getJSON(`http://localhost:8080/comments`)
       .done((data) => {
-        console.log(data.comments);
+        upvotes = data.upvotes;
         data.comments.forEach((comment) => addCommentFromWeb(comment));
       })
       .fail((error) => {});

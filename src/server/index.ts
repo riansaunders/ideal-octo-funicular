51fc70496f78f75364f7431ea1313ded703ed27a
riansaunders <rian@riansaunders.com>
2022-07-16 12:06:50 -0400

    + Modified commentSchema to better match the expected input + Added Comment type + Added default comment + Added json parser to express + Added POST /comment endpoint with validation + Added humanize-duration to the frontend + Updated styling to closer match the reference.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,31 +1,17 @@
 import "dotenv/config";
 import express, { Express, Request, Response } from "express";
 import { exec } from "child_process";
+import * as yup from "yup";
+
+import { faker } from "@faker-js/faker";
 
 const app: Express = express();
 const port = process.env.PORT ?? 8080;
 const isDev = process.env.NODE_ENV !== "production";
 
-const comments: Comment[] = [
-  {
-    author: {
-      name: "Rob Hope",
-      avatarURL: "",
-    },
-    text: "Jeepers now that's a huge release with some big community earnings to back it - it must be so rewarding seeing creators quit their day jobs after monetizing (with real MRR) on the new platform.",
-    upvotes: 0,
-    dateAdded: new Date(),
-  },
-  {
-    author: {
-      name: "Sophie Brecht",
-      avatarURL: "",
-    },
-    text: "Switched our blog from Hubspot to Ghost a year ago -- turned out to be a great decision. Looking forward to this update....the in-platform analytics look especially delicious. :)",
-    upvotes: 0,
-    dateAdded: new Date(),
-  },
-];
+const commentSchema = yup.object({
+  text: yup.string().required(),
+});
 
 type Comment = {
   author: {
@@ -36,6 +22,17 @@ type Comment = {
   upvotes: number;
   dateAdded: Date;
 };
+const comments: Comment[] = [
+  {
+    author: {
+      name: "Rian Saunders",
+      avatarURL: "https://avatars.githubusercontent.com/u/56507515?v=4",
+    },
+    text: "This is my forever comment, in V1",
+    upvotes: 0,
+    dateAdded: new Date(),
+  },
+];
 
 // Since the front-end is plain-jane js, we want to build the web
 // We will be using nodemon's reloading to achieve this.
@@ -43,10 +40,35 @@ if (isDev) {
   exec("npm run build:web");
 }
 
+app.use(express.json());
+
 app.get("/comments", (req, res) => {
   return res.json({
     comments: comments,
   });
+});
+
+app.post("/comment", (req, res) => {
+  try {
+    const { text } = commentSchema.validateSync(req.body);
+
+    const newComment: Comment = {
+      author: {
+        name: faker.name.findName(),
+        avatarURL: faker.image.avatar(),
+      },
+      text: text,
+      upvotes: 0,
+      dateAdded: new Date(),
+    };
+    comments.push(newComment);
+    return res.status(200).json({
+      comment: newComment,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(400).end();
+  }
 });
 
 app.use(express.static("build/web"));
--- src/web/index.js
+++ src/web/index.js
@@ -1,15 +1,33 @@
 /** @type {import("./scripts/jquery-3.6.0.min.js")} */
 import mustache from "./scripts/mustache.js";
 
+console.log(humanizeDuration);
+
 const commentTemplate = `
   <div class="h-20 flex">
-    <img src="{{image}}" />
+    <img src="{{image}}" class="rounded-full h-12 w-12 mr-3" />
     <div class="flex-1 space-y-2">
       <div class="space-x-1">
         <span class="font-semibold">{{name}}</span>
-        <span class="text-gray-400 text-sm">・45 min ago</span>
+        <span class="text-gray-400 text-sm">・{{dateAdded}} ago</span>
       </div>
       <p class="text-sm">{{text}}</p>
+      <div class="space-x-4 text-[#4B587C] text-sm font-semibold">
+        <span>
+          <button>
+
+            <svg class="inline mr-1" width="11" height="10" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0.431818 7L4.14205 0.818182L7.84943 7H0.431818Z" fill="#4B587C"/>
+            </svg>
+            Upvote
+
+          </button>
+        </span>
+        <button>
+          Reply
+        </button>
+
+      </div>
     </div>
   </div>
 `;
@@ -20,33 +38,63 @@ $(function () {
 
   commentForm.on("submit", (e) => {
     e.preventDefault();
-    const { comment } = commentForm.serializeArray();
-    if (!comment) {
+    const commentInput = $("[name='comment']");
+
+    if (!commentInput.val()) {
       alert("Please enter a comment :-)");
       return;
     }
-    $.post(
-      "http://localhost:8080/comment",
-      {
-        //
+    const comment = String(commentInput.val());
+
+    $.ajax({
+      url: "http://localhost:8080/comment",
+      method: "POST",
+      dataType: "json",
+      data: JSON.stringify({
+        text: comment,
+      }),
+      headers: {
+        "Content-Type": "application/json",
       },
-      (success) => {
-        //
-      }
-    );
+      success: (data) => {
+        addCommentFromWeb(data.comment);
+        commentInput.val("");
+      },
+      fail: (error) => {},
+    });
   });
 
-  function addComment({ image, name, text, upvotes }) {
+  function createCommentElement({ image, name, text, upvotes, date }) {
     const element = $(
       mustache.render(commentTemplate, {
         image: image,
         name: name,
         text: text,
         upvotes: upvotes,
+        dateAdded: date,
       })
     );
 
-    commentContainer.append(element);
+    return element;
+  }
+
+  function addCommentFromWeb(comment) {
+    const difference = Date.now() - new Date(comment.dateAdded).getTime();
+
+    const element = createCommentElement({
+      image: comment.author.avatarURL,
+      name: comment.author.name,
+      text: comment.text,
+      upvotes: comment.upvotes,
+      date:
+        difference < 10000
+          ? "moments"
+          : humanizeDuration(difference, {
+              round: true,
+              conjunction: " and ",
+            }),
+    });
+    commentContainer.prepend(element);
     return element;
   }
 
@@ -54,18 +102,9 @@ $(function () {
     $.getJSON("http://localhost:8080/comments")
       .done((data) => {
         console.log(data.comments);
-        data.comments.forEach((comment) => {
-          addComment({
-            image: comment.author.imageURL,
-            name: comment.author.name,
-            text: comment.text,
-            upvotes: comment.upvotes,
-          });
-        });
+        data.comments.forEach((comment) => addCommentFromWeb(comment));
       })
-      .fail((error) => {
-        //
-      });
+      .fail((error) => {});
   }
 
   loadComments();
